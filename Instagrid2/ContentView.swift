241c0db09bd538c8b9b6fcdbb057ae54175752bf
Hquart <hquart@icloud.com>
2020-12-27 15:25:17 +0100

    Initial Commit

---
+++ Instagrid2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instagrid2
+//
+//  Created by Naji Achkar on 27/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instagrid2/Instagrid2App.swift
@@ -0,0 +1,17 @@
+//
+//  Instagrid2App.swift
+//  Instagrid2
+//
+//  Created by Naji Achkar on 27/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Instagrid2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
